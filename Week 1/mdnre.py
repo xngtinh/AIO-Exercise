ad3204cfdd9136185e3a53cfb0c6bba20a94280b
xngtinh <4401104196@student.hcmue.edu.vn>
2024-06-09 21:57:03 +0900

    feature/activation-function - Add activation function

---
+++ Week 1/mdnre.py
@@ -0,0 +1,11 @@
+import sys
+import math
+import random
+
+# Calculate Mean difference of n_th root error
+def md_nre_single_sample(y, y_hat, n, p):
+    value = (y ** (1/n) - y_hat ** (1/n)) ** p
+    print(value)
+
+if __name__ == "__main__":
+    md_nre_single_sample(y=50, y_hat=49.5, n=2, p=1)
